61503aee9b34ac2bccc0be0dddf40e8bc2d1148c
ajitjha393 <ajitjha393@gmail.com>
2022-11-10 12:00:33 +0530

    `timeIt` for tracking multiple transformations

---
+++ src/transform.js
@@ -0,0 +1,43 @@
+﻿const { timeIt, arrayFromRandoms, multiplyByThree, isEven } = require('./utils.js')
+
+const arrayOfThousand = arrayFromRandoms(100)(1e3)
+const arrayOfMillion = arrayFromRandoms(100)(1e7)
+
+// Declarative Transformations
+timeIt('Thousands -> map', () => {
+    arrayOfThousand
+    .map(multiplyByThree)
+})
+
+timeIt('Millions -> map', () => {
+    arrayOfMillion
+    .map(multiplyByThree)
+})
+
+timeIt('Thousands -> map & filter', () => {
+    arrayOfThousand
+    .map(multiplyByThree)
+    .filter(isEven)
+})
+
+timeIt('Millions -> map & filter', () => {
+    arrayOfMillion
+    .map(multiplyByThree)
+    .filter(isEven)
+})
+
+
+// Imperative Transformations
+timeIt('Millions -> map & filter (Imperative)', () => {
+    const result = []
+    arrayOfMillion
+    .forEach(
+        x => {
+            const value = multiplyByThree(x)
+            if(isEven(value)) {
+                result.push(value)
+            }
+        }
+    )
+})
+
