4bf8fbac8eebc0a181aaba845fa717479c4fab76
ajitjha393 <ajitjha393@gmail.com>
2022-11-10 11:59:05 +0530

    intial setup

---
+++ src/utils.js
@@ -0,0 +1,26 @@
+﻿const timeIt = (label, fn) => {
+    console.time(label)
+    fn()
+    console.timeEnd(label)
+}
+
+const arrayFromRandoms = 
+     randomCeil =>
+     length => 
+     Array.from({length: length}, () => 
+     Math.floor(Math.random() * randomCeil))
+
+const isEven = num => !(num % 2)
+
+const multiplyByThree = num => num * 3
+
+const multiplyByTwo = num => num * 2
+
+
+module.exports = {
+    timeIt,
+    arrayFromRandoms,
+    isEven,
+    multiplyByThree,
+    multiplyByTwo
+}
