4d747be8c6a1850009c0774f5949effccba77a49
ajitjha393 <ajitjha393@gmail.com>
2022-11-10 15:20:48 +0530

    create map and filter transducers

---
+++ src/transducers.js
@@ -0,0 +1,31 @@
+﻿const { multiplyByTwo, isEven} = require('./utils.js')
+
+// Transducer => Transform + Reducer
+
+const map = xf => reducer => {
+    return (acc, val) => reducer(acc, xf(val))
+}
+
+const filter =  predicate => reducer => {
+    return (acc, val) => predicate(val) ? reducer(acc, val) : acc
+}
+
+// Defining the maps, filters, and reducers 
+
+const isEvenFilter = filter(isEven)
+const isNotTwoFilter = filter(x => x !== 2)
+
+const doubleMap=  map(multiplyByTwo)
+
+const pushReducer = (acc, val) => [...acc, val]
+
+const result = [1,2,3,4].reduce(
+    isNotTwoFilter(
+     isEvenFilter(
+      doubleMap(pushReducer)
+     )
+    )
+    , []
+)
+
+console.log(result)
